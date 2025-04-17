130f6923d6ecd617f5537b0c68356244962e6569
quyue <10676114@qq.com>
2025-04-17 16:27:47 +0800

    refactor(envSan): 重构车辆基本信息弹窗组件
    - 通过 props 传递车辆信息，提高组件的可复用性

--- src/views/maintenancePlatform/envSan/mapView/components/BasicInfoDialog/BasicInfoDialog.vue
+++ src/views/maintenancePlatform/envSan/mapView/components/BasicInfoDialog/BasicInfoDialog.vue
@@ -1,102 +1,100 @@
 <template>
-    <div class="car-info-pop">
-      <div class="title">
-        <span> 车辆基本信息</span>
-        <i class="icon" @click="closeInfoWindow"></i>
-      </div>
-      <div class="content" v-if="true">
-        <p class="item">
-          <span class="label">车辆编号:</span>
-          <span class="value">{{  '暂无' }}</span>
-        </p>
-        <p class="item">
-          <span class="label">产权单位:</span>
-          <span class="value">{{ '上海浦养环境服务有限公司' }}</span>
-        </p>
-        <p class="item">
-          <span class="label">使用单位:</span>
-          <span class="value">{{ '泥城中转站' }}</span>
-        </p>
-        <p class="item">
-          <span class="label">车牌号码:</span>
-          <span class="value">{{ '沪DF4589' }}</span>
-        </p>
-        <p class="item">
-          <span class="label">车辆类型:</span>
-          <span class="value">{{ '25吨拉臂车' }}</span>
-        </p>
-      </div>
-      <div v-else class="content">该车辆数据暂无...</div>
+  <div class="car-info-pop">
+    <div class="title">
+      <span>车辆基本信息</span>
+      <i class="icon" @click="closeInfoWindow"></i>
     </div>
-  </template>
-  
-  <script setup>
-    import useEnvSanStore from "@/store/modules/envSan.js";
+    <div v-if="infoList.length > 0" class="content">
+      <p v-for="(item, index) in infoList" :key="index" class="item">
+        <span class="label">{{ item.label }}:</span>
+        <span class="value">{{ item.content }}</span>
+      </p>
+    </div>
+    <div v-else class="empty">该车辆数据暂无...</div>
+  </div>
+</template>
 
-    const envSanStore = useEnvSanStore();
-    //关闭弹框
-    const closeInfoWindow = ()=>{
-      envSanStore.closeBasicPointerShow()
-    }
+<script setup>
+import useEnvSanStore from "@/store/modules/envSan.js";
 
-  </script>
-  
-  <style lang="scss" scoped>
-  .car-info-pop {
-    width: 600px;
-    height: 350px;
-    background: url('@/assets/images/visualCockpit/map/centre_pop.png') no-repeat;
-    background-size: 100% 100%;
-    transform: scale(0.6);
-    transform-origin: bottom center;
-  }
-  
-  .car-info-pop .title {
-    width: 100%;
-    height: 70px;
-    font-size: 32px;
-    font-family: YouSheBiaoTiHei;
-    font-weight: 400;
-    color: #fff;
-    line-height: 24px;
-    padding: 0px 30px;
-    padding-top: 5px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  .title .icon {
-    display: block;
-    width: 30px;
-    height: 30px;
-    background: url('@/assets/images/visualCockpit/map/clear_icon.png') no-repeat;
-    background-size: 100% 100%;
-    cursor: pointer;
-  }
-  
-  .car-info-pop .content {
-    width: 100%;
-    height: 280px;
-    font-size: 28px;
-    padding: 0 20px;
-    padding-left: 30px;
-    color: #fff;
-    letter-spacing: 2px;
-  }
-  
-  .content .item {
-    font-size: 22px;
-    height: 30px;
-    transform: skewX(-5deg);
-    font-weight: 700;
-  }
-  
-  .item .value {
-    display: inline-block;
-    width: 300px;
-    color: #28b3b3;
-    white-space: nowrap;
-    margin-left: 10px;
-  }
-  </style>
+const envSanStore = useEnvSanStore();
+
+// 定义 props
+const props = defineProps({
+  infoList: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+// 关闭弹框
+const closeInfoWindow = () => {
+  envSanStore.closeBasicPointerShow();
+};
+</script>
+
+<style lang="scss" scoped>
+.car-info-pop {
+  width: 600px;
+  height: 350px;
+  background: url("@/assets/images/visualCockpit/map/centre_pop.png") no-repeat;
+  background-size: 100% 100%;
+  transform: scale(0.6);
+  transform-origin: bottom center;
+}
+
+.car-info-pop .title {
+  width: 100%;
+  height: 70px;
+  font-size: 32px;
+  font-family: YouSheBiaoTiHei;
+  font-weight: 400;
+  color: #fff;
+  line-height: 24px;
+  padding: 0px 30px;
+  padding-top: 5px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.title .icon {
+  display: block;
+  width: 30px;
+  height: 30px;
+  background: url("@/assets/images/visualCockpit/map/clear_icon.png") no-repeat;
+  background-size: 100% 100%;
+  cursor: pointer;
+}
+
+.car-info-pop .content,.empty {
+  width: 100%;
+  height: 280px;
+  font-size: 28px;
+  padding: 0 20px;
+  padding-left: 30px;
+  color: #fff;
+  letter-spacing: 2px;
+}
+
+.empty{
+ display: flex;
+ justify-content: center;
+ align-items: center;
+}
+
+.content .item {
+  font-size: 22px;
+  height: 30px;
+  transform: skewX(-5deg);
+  font-weight: 700;
+}
+
+.item .value {
+  display: inline-block;
+  width: 300px;
+  color: #28b3b3;
+  white-space: nowrap;
+  margin-left: 10px;
+}
+</style>
